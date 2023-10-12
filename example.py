09f8930698173d7a72c882ccba871f7e976fa927
Anthony Mack <mackattack713@gmail.com>
2023-10-12 15:41:28 +0000

    Created and added the example.py file to the remote repo.

---
+++ example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Google IT Automation with Python - Git and GitHub, Qwiklabs Assessment Week 3
+
+def git_operation():
+	print("I am adding example.py file to the remote repository.")
+
+git_operation()
+
